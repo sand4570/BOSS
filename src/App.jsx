6c5a5896ca42f06723bef621591c4749b95c2967
Chri704r <christinepiilmann8@gmail.com>
2022-05-10 10:29:57 +0200

    about page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Contact from './pages/Contact/index'
 import Login from './pages/Login/index'
 import Footer from './components/Footer/index'
 import Navigation from './components/Navigation'
+import About from './pages/About/index'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 
 function App () {
@@ -16,6 +17,7 @@ function App () {
       <Navigation/>
       <main>
         <Routes>
+        <Route exact path='about' element={<About/>} />
           <Route exact path='contact' element={<Contact/>} />
           <Route exact path='login' element={<Login/>} />
           <Route exact path='/' element={<Frontpage/>} />
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './style.scss'
+
+const About = () => {
+    return (
+        <>
+        <img id='form1' src='/form1.svg'></img>
+        <main>
+            <section id='about_section'>
+            <div>
+            <h2>Hvem er Boss</h2>
+            <p>BOSS Informatik blev startet i 1992 og har i alle årene været det mest anvendte system til VUC’er, Gymnasier og Erhvervsskoler…</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est.</p>
+            </div>
+            <img src='/book_scan.png' alt='bog bliver scannet'></img>
+            </section>
+            <section id='owner_section'>
+                <div id='portrait_wrapper'>
+                    <img src='/portrait_placeholder.jpg'></img>
+                    <div id='owner_desk_wrapper'>
+                        <p className='citat'>“ Det der er værd at gøre, er også værd at gøre godt “</p>
+                        <p className='citat_afsender'>Torben Piilmann</p>
+                    </div>
+                </div>
+                <div id='torben_wrapper'>
+                    <h2>Torben Piilmann</h2>
+                    <p>Torben er ejer af BOSS og manden der sørger for at systemet virker. Torben har uddannelse i søværnet som elektronik mekaniker, og er uddannet elektroniktekniker på Frederiksberg teknisk skole. Herudover har han i 2020 taget kursus i Cloud teknologi og database på CPH Business. Torben i mange år arbejdet som tekniker, hvor han har arbejdet med software og hardware. Inden han ejede BOSS arbejdede han hos TagVision som teknisk chef, og samarbejdede de sidste 10 år sammen med BOSS.Torben er en udpræget ‘gør det selv’ mand, som grundet gå på mod og nysgerrighed kan alt fra at undervise, til at bygge et hus eller reparere en bil eller båd. Hans mission er at modernisere og videreudvikle BOSS så systemet bliver mere brugervenligt. </p>
+                </div>
+                <img id='work_img' src='/placeholder.jpg'></img>
+            </section>
+            <section id='vision_section'>
+                <div id='vision_wrapper'>
+                <h2>Visioner for boss</h2>
+                <p>BOSS har en vision om at flytte systemet til en moderne platform, hvor databasen bliver Cloud baseret, og data dermed ligger i skyen. Dette betyder at der vil være minimal vedligholdelse fra brugerens side og systemet automatisk vil blive opdateret. Du skal som bruger derfor ikke længere bruge tid på at opdatere, og opleve eventuelle udfordringer med dette. Visionen er dermed at BOSS systemet skal gøres moderne, så du som bruger får lettere ved at benytte systemet. </p>
+                </div>
+                <img src='/vision_grafik.png'></img>
+            </section>
+        </main>
+        <img id='form2' src='form2.svg'></img>
+        </>
+    )
+}
+
+export default About
