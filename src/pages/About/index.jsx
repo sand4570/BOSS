26ae0f1707c2da93bd2e30826d38f7b7c1a37e65
Chri704r <christinepiilmann8@gmail.com>
2022-05-24 20:25:21 +0200

    about styling

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -16,16 +16,18 @@ const About = () => {
             <img src='/bog_scan2.jpg' alt='bog bliver scannet'></img>
             </section>
             <section id='owner_section'>
-                <div id='portrait_wrapper'>
-                    <img src='/torben-portrait.jpg'></img>
-                    <div id='owner_desk_wrapper'>
-                        <p className='citat'>“ Det der er værd at gøre, er også værd at gøre godt “</p>
-                        <p className='citat_afsender'>Torben Piilmann</p>
-                    </div>
+            <div id='owner_desk_wrapper'>
+                    <p className='citat'>“ Det der er værd at gøre, er også værd at gøre godt “</p>
+                    <p className='citat_afsender'>Torben Piilmann</p>
                 </div>
+            <img id="portrait" src='/torben-portrait.jpg'></img>
                 <div id='torben_wrapper'>
+                    <div>
                     <h3>Torben Piilmann</h3>
-                    <p>Torben er ejer af BOSS og manden der sørger for at systemet virker. Torben har uddannelse i søværnet som elektronik mekaniker, og er uddannet elektroniktekniker på Frederiksberg teknisk skole. Herudover har han i 2020 taget kursus i Cloud teknologi og database på CPH Business. Torben i mange år arbejdet som tekniker, hvor han har arbejdet med software og hardware. Inden han ejede BOSS arbejdede han hos TagVision som teknisk chef, og samarbejdede de sidste 10 år sammen med BOSS. Torben er en udpræget ‘gør det selv’ mand, som grundet gå på mod og nysgerrighed kan alt fra at undervise, til at bygge et hus eller reparere en bil eller båd. Hans mission er at modernisere og videreudvikle BOSS så systemet bliver mere brugervenligt. </p>
+                    <p>Torben er ejer af BOSS og manden der sørger for at systemet virker. Torben har uddannelse i søværnet som elektronik mekaniker, og er uddannet elektroniktekniker på Frederiksberg teknisk skole. Herudover har han i 2020 taget kursus i Cloud teknologi og database på CPH Business. </p>
+                    <p>Torben i mange år arbejdet som tekniker, hvor han har arbejdet med software og hardware. Inden han ejede BOSS arbejdede han hos TagVision som teknisk chef, og samarbejdede de sidste 10 år sammen med BOSS.</p>
+                    <p>Torben er en udpræget ‘gør det selv’ mand, som grundet gå på mod og nysgerrighed kan alt fra at undervise, til at bygge et hus eller reparere en bil eller båd. Hans mission er at modernisere og videreudvikle BOSS så systemet bliver mere brugervenligt. </p>
+                    </div>
                 </div>
                 <img id='work_img' src='/torben-present.jpg'></img>
             </section>
